8406f1d5465c103cae95bcb44a6be37b737b5c3c
Nazar Stenin <Mr.Lamzikk@yandex.ru>
2025-03-29 13:40:53 +0300

    #

--- back/api/app.py
+++ back/api/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.params import Depends
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
+from db.crud.students import get_user_by_credentials, add_student_token
 
 from db.base import get_db
 
@@ -14,3 +15,13 @@ async def pong(session: AsyncSession = Depends(get_db)):
         res = await session.execute(text("SELECT 1"))
         return {"res": res.scalar_one()}
     # return {"ping": "pong"}
+
+@app.post("/autorisation", tags = ["Авторизация"])
+async def autorisation(login: str,
+                       password: str,
+                       session: AsyncSession = Depends(get_db)):
+        Student = await get_user_by_credentials(login, password, session)
+        if Student == None:
+             raise HTTPException(statuse_code = 401, detail="auth faild")
+        else:
+             return await add_student_token(Student.id, session)
