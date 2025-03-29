bee80a2df8c393b9670706affed89c424eca7835
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-03-29 12:21:25 +0300

    fix

--- back/api/app.py
+++ back/api/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.params import Depends
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.base import get_db
 
 app = FastAPI()
 
 
 @app.get('/ping')
-async def pong():
-    return {"ping": "pong"}
+async def pong(session: AsyncSession = Depends(get_db)):
+    async with session:
+        res = await session.execute(text("SELECT 1"))
+        return {"res": res.scalar_one()}
+    # return {"ping": "pong"}
