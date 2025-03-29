b8f7b77111e0ebdc77363691d28fdee3cb01d255
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-03-29 12:56:34 +0300

    fix

---
+++ back/db/models/timetables.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from db.base import Base
+
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    login: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column()
