2625c56b9fdea66bfc42d2fe50e42c211d271319
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-03-29 13:35:42 +0300

    feat: add func to add token

--- back/db/crud/students.py
+++ back/db/crud/students.py
@@ -1,5 +1,7 @@
+from uuid import uuid4
+
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, update
 from db.models.students import Student
 
 
@@ -15,3 +17,18 @@ async def get_user_by_credentials(
         res = await session.execute(stmt)
 
         return res.scalar_one_or_none()
+
+
+async def add_student_token(student_id: int,
+                            session: AsyncSession) -> str:
+    async with session:
+        token = str(uuid4())
+        stmt = (update(Student)
+                .where(Student.id == student_id)
+                .values(token=token)
+                .returning(Student.token))
+
+        res = await session.execute(stmt)
+        await session.commit()
+
+        return res.scalar_one()
