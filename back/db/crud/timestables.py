339afa1a8b4ae7f3cfcc98935d52dadc93321b0f
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-03-29 14:51:05 +0300

    feat: add timetable getting crud operations

---
+++ back/db/crud/timestables.py
@@ -0,0 +1,30 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.models.timetable import Timetable
+
+
+async def get_class_timetable(
+        class_id: int,
+        session: AsyncSession
+) -> list[Timetable]:
+    async with session:
+        stmt = (select(Timetable)
+                .where(Timetable.class_id == class_id)
+                .order_by(Timetable.day_of_week)
+                .order_by(Timetable.lesson_number))
+        res = await session.execute(stmt)
+
+        return res.scalars().all()
+
+
+async def get_teacher_timetable(teacher_id: int,
+                                session: AsyncSession) -> str:
+    async with session:
+        stmt = (select(Timetable)
+                .where(Timetable.teacher_id == teacher_id)
+                .order_by(Timetable.day_of_week)
+                .order_by(Timetable.lesson_number))
+
+        res = await session.execute(stmt)
+        return res.scalars().all()
